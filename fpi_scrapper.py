2b929bf75f73ff5ca6f3c0cbda0bdfb84216f379
priyanshrm <priyanshmahendra@gmail.com>
2026-06-21 17:42:39 +0530

    second

--- fpi_scrapper.py
+++ fpi_scrapper.py
@@ -4,6 +4,7 @@ import calendar
 from datetime import datetime, timedelta
 
 from selenium import webdriver
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
